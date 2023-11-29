98dd55fd8a6716751e9b47d003d08eed267cd661
Anna-Bohun-art <63401934+Anna-Bohun-art@users.noreply.github.com>
2023-11-29 13:22:37 +0100

    changing res.header

--- server.js
+++ server.js
@@ -21,12 +21,14 @@ app.use("/sensor", sensorRoutes);
 app.use("/user", userRoutes);
 app.use(express.urlencoded({extended: true}));
 app.use((req, res, next)=>{
-    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 })
-
+/*app.use(cors({
+    origin: "http://localhost:8080"
+}));*/
 app.get("/", (req, res)=> {
     res.json("Welcome to sensor API");
 })
