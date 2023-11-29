45dbdfdd577298397058f4073a939885c154889c
Anna-Bohun-art <63401934+Anna-Bohun-art@users.noreply.github.com>
2023-11-29 13:13:20 +0100

    changing res.header for CORS purposes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use("/sensor", sensorRoutes);
 app.use("/user", userRoutes);
 app.use(express.urlencoded({extended: true}));
-app.get((req, res, next){
+app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
